e13cb984cba43d3a876334da3a4b66bc6af678f7
AsgerSP <asger.s.pedersen@gmail.com>
2022-06-01 10:44:59 +0200

    changed disjoint graph to original

--- js/d3-module.js
+++ js/d3-module.js
@@ -1,7 +1,7 @@
 // NOTE: Datadreven supports 3 kinds of D3 charts: chart, canvas & disjoint. This file should be updated for more types
 import { Runtime, Inspector } from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js";
 import define from "https://api.observablehq.com/@asgersp/d3-force-directed-graph-with-input.js?v=3";
-import defineDisjoint from "https://api.observablehq.com/@asgersp/force-directed-graph-disjoint.js?v=3";
+import defineDisjoint from "https://api.observablehq.com/@d3/disjoint-force-directed-graph.js?v=3";
 import defineCanvas from "https://api.observablehq.com/@asgersp/voroblobinoids.js?v=3";
 const d3ChartPlaceholders = document.querySelectorAll(".d3-chart-placeholder");
 
