ae3a40c1c89e99225acf73c9d66b270d26b7e3f8
AsgerSP <asger.s.pedersen@gmail.com>
2022-06-23 21:22:18 +0200

    colors and first blogstarted

--- js/d3-module.js
+++ js/d3-module.js
@@ -1,10 +1,12 @@
 // NOTE: Datadreven supports 3 kinds of D3 charts: chart, canvas & disjoint. This file should be updated for more types
 import { Runtime, Inspector } from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js";
 import define from "https://api.observablehq.com/@asgersp/d3-force-directed-graph-with-input.js?v=3";
-import defineDisjoint from "https://api.observablehq.com/@d3/disjoint-force-directed-graph.js?v=3";
+import defineDisjoint from "https://api.observablehq.com/@asgersp/force-directed-graph-disjoint@267.js?v=3";
+import defineDisjointSmall from "https://api.observablehq.com/d/85654f9589f63b8f.js?v=3";
 import defineCanvas from "https://api.observablehq.com/@asgersp/voroblobinoids.js?v=3";
 import defineSpiral from "https://api.observablehq.com/@mcmcclur/self-similar-spirals.js?v=3";
 import defineSticky from "https://api.observablehq.com/@mcmcclur/iterated-function-systems.js?v=3";
+import defineBubblechart from "https://api.observablehq.com/d/139422bced734dbe.js?v=3";
 const d3ChartPlaceholders = document.querySelectorAll(".d3-chart-placeholder");
 
 d3ChartPlaceholders.forEach(function (d3ChartPlaceholder) {
@@ -23,6 +25,11 @@ d3ChartPlaceholders.forEach(function (d3ChartPlaceholder) {
             if (name === "chart") return new Inspector(d3ChartPlaceholder);
             return ["key"].includes(name);
         });
+    } else if (d3Type == "disjointsmall") {
+        new Runtime().module(defineDisjointSmall, name => {
+            if (name === "chart") return new Inspector(d3ChartPlaceholder);
+            return ["key"].includes(name);
+        });
     } else if (d3Type == "spiral") {
         new Runtime().module(defineSpiral, name => {
             if (name === "interactive_spiral") return new Inspector(d3ChartPlaceholder);
@@ -30,6 +37,10 @@ d3ChartPlaceholders.forEach(function (d3ChartPlaceholder) {
     } else if (d3Type == "sticky") {
         new Runtime().module(defineSticky, name => {
             if (name === "sticky_sierpinski") return new Inspector(d3ChartPlaceholder);
+        });
+    } else if (d3Type == "bubblechart") {
+        new Runtime().module(defineBubblechart, name => {
+        if (name === "chart") return new Inspector(d3ChartPlaceholder);
         });
     }
     
